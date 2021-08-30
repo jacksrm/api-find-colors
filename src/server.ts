e0f38cc31b0fe273764d00497439f17c9a8e0699
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-08-30 09:46:34 -0300

    Adiciona Jest e configura TS com Jest

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req,res) => res.json({ message: 'Hello World' }));
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
-app.listen(3333, () => { console.log('App running on http://localhost:3333') }); 
+app.listen(3333, () => { console.log('App running on http://localhost:3333'); });
