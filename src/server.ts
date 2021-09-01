f6fb35b28d9c94ab12ce267b765ef3d25704e184
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-01 18:22:07 -0300

    Ajusta arquivo inicial

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req: Request, res: Response) =>
   res.json({ message: 'Hello World' }),
 );
