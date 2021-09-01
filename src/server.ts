20c180312199f7ed4aed903de28de3b50482ca19
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-01 19:03:06 -0300

    Implementa retorno de todas as cores;

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  const coresDataFile = fs.readFileSync(`${__dirname}/coresData.json`, { encoding: 'utf-8'});
+  const coresDataFile = fs.readFileSync(`${__dirname}/coresData.json`, {
+    encoding: 'utf-8',
+  });
   const coresData = JSON.parse(coresDataFile);
 
   return res.status(200).json({ cores: coresData });
