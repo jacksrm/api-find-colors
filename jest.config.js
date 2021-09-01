361fac2f9f725c11817f6a57f691a8fd03d6da07
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-01 17:26:24 -0300

    Projeto inicial.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-const { compilerOptions } = require('./tsconfig.json');
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' })
-};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
-import soma from '@utils/soma';
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Hello World', soma: soma(10, 12) }));
+app.get('/', (req: Request, res: Response) =>
+  res.json({ message: 'Hello World' }),
+);
 
-app.listen(3333, () => { console.log('App running on http://localhost:3333'); });
+app.listen(3333, () => {
+  console.log('App running on http://localhost:3333');
+});
