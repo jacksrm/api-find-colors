99a5080d9d2804c4d4773f8f0d427a6296b5d3b4
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-09-01 18:47:55 -0300

    Cria rota que indexa todas as cores

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from 'express';
+import fs from 'fs';
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) =>
-  res.json({ message: 'Hello World' }),
-);
+app.get('/', (req: Request, res: Response) => {
+  const coresDataFile = fs.readFileSync(`${__dirname}/coresData.json`, { encoding: 'utf-8'});
+  const coresData = JSON.parse(coresDataFile);
+
+  return res.status(200).json({ cores: coresData });
+});
 
 app.listen(3333, () => {
   console.log('App running on http://localhost:3333');
